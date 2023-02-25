7e5d56bdff8db12a30aeb5ce617ed4a8781e14f5
reeteshkesarwani <kesarwani.r@northeastern.edu>
2023-02-25 00:35:25 -0500

    Creating complex form and handling using Model Attribute

---
+++ springmvc/src/main/java/springmvc/controller/FormController.java
@@ -0,0 +1,24 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FormController {
+
+    @RequestMapping("/complex")
+    public String showForm(){
+
+        return "complex_form";
+    }
+
+    @RequestMapping(path ="/handleform",method = RequestMethod.POST)
+    public String formHandle(@ModelAttribute("student") Student student){
+
+        System.out.println(student);
+        return "success2";
+    }
+
+}
